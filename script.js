c67899e49aa5f0c2b462ef7157eba4457b944df0
entuelias <intuelias07@gmail.com>
2025-03-02 00:33:03 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleMenu = document.querySelector(".toggle-menu");
+    const navLinks = document.querySelector(".nav-links");
+  
+    toggleMenu.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  });
+  
